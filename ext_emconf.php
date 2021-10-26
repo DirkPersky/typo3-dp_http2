d35d8171ad3284a4a2aac5abcba5b0e929f96232
Dirk.Persky <dirk.persky@gmail.com>
2021-10-26 10:35:16 +0200

    TYPO3 11 test

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.1.99'
+            'typo3' => '9.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [
